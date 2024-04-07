3770aab7a62a7335e5be0681c32d3e9121b36208
KiraSeletska <seletskaya.kira@gmail.com>
2024-04-07 14:42:33 +0200

    add: active link

--- portfolio-test/src/app/left-bar/links-list/links-list.component.ts
+++ portfolio-test/src/app/left-bar/links-list/links-list.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { Component, ElementRef } from '@angular/core';
 import { ProjectsComponent } from '../../projects/projects.component';
 import { SkillsComponent } from '../../skills/skills.component';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule} from '@angular/router';
+
 @Component({
   selector: 'app-links-list',
   standalone: true,
-  imports: [MatSidenavModule, ProjectsComponent, SkillsComponent, RouterModule],
+  imports: [ProjectsComponent, SkillsComponent, RouterModule],
   templateUrl: './links-list.component.html',
   styleUrl: './links-list.component.scss'
 })
 
 export class LinksListComponent {
+
 
 }
 
