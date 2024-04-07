c656bc7549910835f2a54c960c5795e7d32cee85
KiraSeletska <seletskaya.kira@gmail.com>
2024-04-07 13:15:02 +0200

    add: router

--- portfolio-test/src/app/content-panel/content-panel.component.ts
+++ portfolio-test/src/app/content-panel/content-panel.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProjectsComponent } from '../projects/projects.component';
+import { ExperienceComponent } from '../experience/experience.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { ContactsComponent } from '../contacts/contacts.component';
 
 @Component({
   selector: 'app-content-panel',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ProjectsComponent, ExperienceComponent, SkillsComponent, ContactsComponent],
   templateUrl: './content-panel.component.html',
   styleUrl: './content-panel.component.scss'
 })
--- portfolio-test/src/app/left-bar/links-list/links-list.component.ts
+++ portfolio-test/src/app/left-bar/links-list/links-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ProjectsComponent } from '../../projects/projects.component';
+import { SkillsComponent } from '../../skills/skills.component';
+import { RouterModule, Routes } from '@angular/router';
 @Component({
   selector: 'app-links-list',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, ProjectsComponent, SkillsComponent, RouterModule],
   templateUrl: './links-list.component.html',
   styleUrl: './links-list.component.scss'
 })
+
 export class LinksListComponent {
 
 }
+
