2ea2bf6d42bdefad958dffc9630efef65da1d09a
KiraSeletska <seletskaya.kira@gmail.com>
2024-04-22 18:52:19 +0200

    add: projects

--- portfolio-test/src/app/data-serices/projects.service.ts
+++ portfolio-test/src/app/data-serices/projects.service.ts
@@ -5,20 +5,39 @@ import { SkillsIcons } from '../pages/experience/experience.component';
   providedIn: 'root',
 })
 export class ProjectService {
+  skillsIcons: SkillsIcons = {
+    Typescript: '../../../assets/icons/icons_scills/typescript.svg',
+    Javascript: '../../../assets/icons/icons_scills/javascript.svg',
+    SASS: '../../../assets/icons/icons_scills/sass.svg',
+    React: '../../../assets/icons/icons_scills/react.svg',
+    Angular: '../../../assets/icons/icons_scills/angular.svg',
+    MobX: '../../../assets/icons/icons_scills/mobx.svg',
+    Redux: '../../../assets/icons/icons_scills/redux.svg',
+    Figma: '../../../assets/icons/icons_scills/figma.svg',
+  };
 
-    skillsIcons: SkillsIcons = {
-        'Typescript': '../../../assets/icons/icons_scills/typescript.svg',
-        'Javascript': '../../../assets/icons/icons_scills/javascript.svg',
-        'SASS': '../../../assets/icons/icons_scills/sass.svg',
-        'React': '../../../assets/icons/icons_scills/react.svg',
-        'Angular': '../../../assets/icons/icons_scills/angular.svg',
-        'MobX': '../../../assets/icons/icons_scills/mobx.svg',
-        'Redux': '../../../assets/icons/icons_scills/redux.svg',
-        'Figma': '../../../assets/icons/icons_scills/figma.svg',
+  getSkillsIcons() {
+    return this.skillsIcons;
+  }
 
-      };
-
-      getSkillsIcons(){
-        return this.skillsIcons
-      }
+  linksToMainImages = [
+    'assets/images/projects/game/2.png',
+    'assets/images/projects/ll/LL.png',
+    'assets/images/projects/portfolio/1.png',
+    'assets/images/projects/store/1.png',
+    'assets/images/projects/todo/todo2.png',
+  ];
+  getLinks() {
+    return this.linksToMainImages;
+  }
+  descrMainImages = [
+    { link: 'assets/images/projects/game/2.png', descr: 'Online game "Space Expatriate" for 2 or more players', skills: ['Typescript', 'React', 'MobX', 'SASS'] },
+    { link: 'assets/images/projects/ll/LL.png', descr: 'Online application for learning languages', skills: ['Javascript', 'SASS', 'React'] },
+    { link: 'assets/images/projects/portfolio/1.png', descr: 'UI/UX Designer Portfolio', skills: ['Typescript', 'SASS', 'React', 'Figma'] },
+    { link: 'assets/images/projects/store/1.png', descr: 'Onlineshop Garten', skills: ['Javascript', 'SASS', 'React'] },
+    { link:  'assets/images/projects/todo/todo2.png', descr: 'Todo list', skills: ['Typescript', 'SASS'] },
+  ];
+  getDescrMainImages(){
+    return this.descrMainImages
+  }
 }
--- portfolio-test/src/app/pages/projects/projects.component.ts
+++ portfolio-test/src/app/pages/projects/projects.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProjectService } from '../../data-serices/projects.service';
 
+interface MainImages{
+  link: string;
+  descr: string;
+  skills: []
+}
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit  {
+  imagesLinks: any;
+mainImages!: any;
+
+  constructor(
+    private imgLinks: ProjectService,
+    private imagesInfo: ProjectService
+  ) {}
+  ngOnInit(): void {
+    this.imagesLinks = this.imgLinks.getLinks();
+    this.mainImages = this.imagesInfo.getDescrMainImages();
+  }
 
 }
