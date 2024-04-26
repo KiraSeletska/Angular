2a8120e193a1f90ca63f6eef82d8fa073244219d
KiraSeletska <seletskaya.kira@gmail.com>
2024-04-26 11:58:03 +0200

    .

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { gsap } from 'gsap';
 import { BoxesComponent } from './boxes/boxes.component';
 import { HeaderComponent } from './header/header.component';
+import { CubeComponent } from './cube/cube.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BoxesComponent, HeaderComponent],
+  imports: [RouterOutlet, BoxesComponent, HeaderComponent, CubeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ animations/src/app/cube/cube.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
+import { gsap } from 'gsap';
+
+@Component({
+  selector: 'app-cube',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cube.component.html',
+  styleUrl: './cube.component.scss'
+})
+export class CubeComponent implements AfterViewInit {
+  rots = [
+    { ry: 0,   rx: 0  }, // 1
+    { ry: 90,  rx: 0  }, // 2
+    { ry: 180, rx: 0  }, // 3
+    { ry: 270, rx: 0  }, // 4
+    { ry: 0,   rx: 90 }, // 5
+    { ry: 0,   rx:-90 }  // 6
+  ];
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    gsap.set(".face", {
+      rotateY: (i) => this.rots[i].ry,
+      rotateX: (i) => this.rots[i].rx,
+      transformOrigin: "50% 50% -150px",
+      z: 150,
+     // background:(i) => 'url(https://assets.codepen.io/721952/dieSprite_1.svg) 0px -' + String(i * 300) + 'px'
+    });
+
+    gsap.timeline({ repeat: -1, defaults: { duration: 1, ease: "expo.inOut" } })
+      .to('#cube', { rotateX: 0,  rotateY: -90  })
+      .to('#cube', { rotateX: 0,  rotateY: -180 })
+      .to('#cube', { rotateX: 0,  rotateY: -270 })
+      .to('#cube', { rotateX:-90, rotateY: -360 })
+      .to('#cube', { rotateX: 90, rotateY: -360 })
+      .to('#cube', { rotateX: 0,  rotateY: -360 });
+  }
+}
